e7e67784bfd802c9dcac4e2d70aa3f20a39436d5
Miguel Lariz <duronlarizm@gmail.com>
2024-04-23 19:59:12 -0600

    Función de valores aleatorios agregada

--- FIFO C++/reemplazoFIFO.cpp
+++ FIFO C++/reemplazoFIFO.cpp
@@ -10,6 +10,7 @@
 #include <string>
 using namespace std;
 
+
 // Variables globales
 int matriz[ 1000 ][ 1000 ];
 
@@ -19,6 +20,7 @@ void vaciarMatriz( int );
 void imprimirMatriz( int );
 void valoresManual( );
 void leerArchivo( );
+void aleatorios( );
 
 
 int main()
@@ -32,39 +34,50 @@ int main()
 
     do
     {
+        // Limpiar consola cada que se entra
         system( "cls" );
 
+        // Titulo
         cout << "\n Reemplazo FIFO " << endl;
 
         cout << " Menu " << endl;
 
+        // Opciones dentro del algoritmo
         cout << " 1. Llenar los valores manualmente. " << endl;
         cout << " 2. Leerlos de un archivo txt." << endl;
-        cout << " 3. Aleatorios." << endl << endl;
+        cout << " 3. Valores aleatorios." << endl << endl;
 
-    cout << " Opcion: "; cin >> opcion;
+        // Lectura de la opcion seleccionada
+        cout << " Opcion: "; cin >> opcion;
 
+    // Validacion de la opcion
     } while ( opcion != 1 && opcion != 2 && opcion != 3 );
     
-    
+
+    // Mandar a cada opcion
     switch ( opcion )
     {
+
+        // 1. Llenar los valores manualmente.
         case 1:
             valoresManual( );
             break;
 
+        // 2. Leerlos de un archivo.txt.
         case 2:
             leerArchivo( );
             break;
 
-        case 3: break;
+        // 3. Valores aleatorios
+        case 3:
+            aleatorios( );
+            break;
         
-    default:
-        break;
+        default: break;
     }
 
+    // Detener ejecucion
     system( "pause" );
-
 	return 0;	
 }
 
@@ -72,6 +85,7 @@ int main()
 // Vaciar la matriz por si exisitiera valores basura
 void vaciarMatriz( int k )
 {
+    // Recorrer la matriz la cantidad especificada
     for ( int m = 0; m < k; m++ )
     {
         for ( int f = 0; f < k; f++ )
@@ -81,24 +95,35 @@ void vaciarMatriz( int k )
     }
 }
 
+
+// Funcion que permite imprimir la matriz que se esta utilizando
 void imprimirMatriz( int limite )
 {
 
+    // Encabezados de la tabla
     cout << "Proce\tLlega\ttiempo\tPriori\tInicio\t Fin\tTiemTot\tEspera\tTiemPro" << endl;
+
+    // Recorrer tabla la cantidad de procesos
     for ( int i = 0; i < limite; i++ )
     {
+
+        // Imprimir cada renglon de la tabla
         for ( int k = 0; k < 9; k++ )
         {
+
+            // Imprimir por temas de estetica
             if( matriz[ i ][ k ] < 10 ) cout << "  " << matriz[ i ][ k ] << "\t";
             else if( matriz[ i ][ k ] < 100 && matriz[ i ][ k ] >= 10 ) cout << " " << matriz[ i ][ k ] << "\t";
             else cout << matriz[ i ][ k ] << "\t";
         }
 
+        // Separar cada renglon
         cout << endl;
     }
 }
 
 
+// Opcion 1. Introducir los valores manualmente
 void valoresManual( )
 {
     int noProcesos = 0;
@@ -109,45 +134,59 @@ void valoresManual( )
     // Vaciado de los espacios que necesitamos
     vaciarMatriz( noProcesos );
 
+    // Variables que se ocupan y se piden al usuario por cada proceso
     int llegada = 0 , tiempo = 0 , prioridad = 0;
 
+    // Ciclo para pedir datos
     for ( int k = 0; k < noProcesos; k++ )
     {
+
+        // Titulo del Proceso
         cout << " Proceso #" << ( k + 1 ) << endl;
 
         for (int i = 0; i < 9; i++)
         {
+
+            // Para cada caso
             switch ( i )
             {
+
+                // Nombre del proceso
                 case 0:
                     matriz[ k ][ i ] = ( k + 1 );
                     break;
 
+                // Pedir tiempo de llegada
                 case 1:
                     cout << " Llegada: "; cin >> llegada;
                     matriz[ k ][ i ] = llegada;
                     break;
 
+                // Pedir tiempo de que se toma
                 case 2:
                     cout << " Tiempo: " ; cin >> tiempo;
                     matriz[ k ][ i ] = tiempo;
                     break;
 
+                // Pedir prioridad
                 case 3:
                     cout << " Prioridad: "; cin >> prioridad;
                     matriz[ k ][ i ] = prioridad;
                     break;
             
+                // LLenar el resto de la tabla con ceros
                 default:
                     matriz[ k ][ i ] = 0;
                     break;
             }
         }
-        
     }
 
+    cout << "\t Primer tabla de datos " << endl;
+    // Mostrar como quedo la matriz
     imprimirMatriz( noProcesos );
 }
+
 
 // Opcion 2: Leer valores de un archivo externo txt
 void leerArchivo( )
@@ -172,7 +211,7 @@ void leerArchivo( )
     ifstream archivo( nombreArchivo.c_str( ) );
 
     string linea = "" , llegada = "" , tiempo = "" , prioridad = "";
-    int tamanioMatriz = 0;
+    int noProcesos = 0;
     bool espacio1 = true , espacio2 = true;
 
     for ( int k = -1; getline( archivo , linea ); k++ )
@@ -184,7 +223,7 @@ void leerArchivo( )
             continue;
         }
 
-        if( k == -1 ) tamanioMatriz = stoi( linea );
+        if( k == -1 ) noProcesos = stoi( linea );
         else
         {
             for (int i = 0; i < 9; i++)
@@ -216,6 +255,133 @@ void leerArchivo( )
 
     system( "cls" );
     cout << "\t Reemplazo de página FIFO " << endl << endl;
-    imprimirMatriz( tamanioMatriz );
+    imprimirMatriz( noProcesos );
 
 }
+
+
+// Opcion 3. Valores aleatorios
+void aleatorios( )
+{
+
+    // Limpiar consola
+    system( "cls" );
+
+    // Necesario para numeros aleatorios
+    srand(time(NULL));
+
+    // Cantidad de procesos que se tendran
+    int noProcesos = 0;
+    do
+    {
+        cout << " Cantitdad de procesos: "; cin >> noProcesos;
+    } while ( noProcesos <= 0 || noProcesos >= 101);
+    
+    // Separador
+    cout << endl;
+
+    // Variable para seleccionar una opcion
+    int seleccion = 0;
+
+    // Variables que se ocupan y se piden al usuario
+    int maxLlegada = 0 , maxTiempo = 0 , maxPrioridad = 0;
+
+    do
+    {
+        // Menu para limites
+        cout << "\t Opciones " << endl;
+        cout << " 1. Definir limites manualmente. " << endl;
+        cout << " 2. Limites por defecto (Llegada: 0-20, Tiempo: 1-10, Prioridad: 0-5) " << endl;
+
+        // Lectura de seleccion
+        cout << endl << endl << " Seleccion: "; cin >> seleccion;
+
+    // Validacion de seleccion
+    } while ( seleccion != 1 && seleccion != 2 );
+
+    switch ( seleccion )
+    {
+        case 1:
+
+            // Lectura de limites de llegada
+            do
+            {
+                system( "cls" );
+                cout << " Llegada " << endl;
+                cout << " Introduce el valor maximo: "; cin >> maxLlegada;
+            } while ( maxLlegada < 0 || maxLlegada >= 21 );
+
+
+            // Lectura de limites de tiempo
+            do
+            {
+                system( "cls" );
+                cout << " Tiempo " << endl;
+                cout << " Introduce el valor maximo: "; cin >> maxTiempo;
+            } while ( maxTiempo < 1 || maxTiempo >= 11 );
+
+
+            // Lectura de limites de prioridad
+            do
+            {
+                system( "cls" );
+                cout << " Prioridad " << endl;
+                cout << " Introduce el valor maximo: "; cin >> maxPrioridad;
+            } while ( maxPrioridad < 0 || maxPrioridad >= 6 );
+
+            break;
+
+        case 2:
+            
+            // minLlegada = rand() % 21;
+            maxLlegada = 20;
+            maxTiempo = 10;
+            maxPrioridad = 5;
+            break;
+    
+        default: break;
+    }
+
+    system( "cls" );
+    cout << "\t Valores definidos " << endl;
+    cout << " Llegada: 0-" << maxLlegada << endl;
+    cout << " Tiempo: 1-" << maxTiempo << endl;
+    cout << " Prioridad: 0-" << maxPrioridad << endl;
+
+    for ( int i = 0; i < noProcesos; i++)
+    {
+        for ( int k = 0; k < 9; k++)
+        {
+            switch ( k )
+            {
+                // Numero de proceso
+                case 0: matriz[ i ][ k ] = i; break;
+
+                // Generar llegada
+                case 1: matriz[ i ][ k ] = rand( ) % ( maxLlegada + 1 ); break;
+
+                // Generar tiempo
+                case 2: matriz[ i ][ k ] = rand( ) % ( maxTiempo + 1 ); break;
+
+                // Generar prioridad
+                case 3: matriz[ i ][ k ] = rand( ) % ( maxPrioridad + 1 ); break;
+            
+                // Llenar con 0 las demas columnas
+                default: matriz[ i ][ k ] = 0; break;
+            }
+        }
+    }
+
+    // Limpiar consola
+    system( "cls" );
+
+    cout << "\t Tabla con valores aleatorios " << endl;
+    imprimirMatriz( noProcesos );
+
+    // Separador
+    cout << endl;
+}
+
+
+
+
